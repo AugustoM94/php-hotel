7e6c3cc66222e442cc38dd17b630c6f80ca85a46
AugustoM94 <email@github.com>
2023-11-29 15:59:01 +0100

    fix header

--- partials/header.php
+++ partials/header.php
@@ -11,16 +11,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
-
-
-        <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>PHP Hotel</title>
 </head>
 <body>
 <header>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#"><img src="https://it.hotels.com/_dms/header/logo.svg?locale=it_IT&amp;siteid=300000007&amp;2&amp;3ec60977" alt="Logo hotels" class="large-logo">
+</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -30,11 +29,11 @@
           <a class="nav-link active" aria-current="page" href="#">Accedi</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Assistenza</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Stanze
+            Prenotazioni
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Singola</a></li>
